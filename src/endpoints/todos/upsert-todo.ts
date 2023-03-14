db66c6f25ee21c7f7948962a55db31614d5e6791
M. Gacuma <miguel_gacuma@yahoo.com>
2023-03-13 18:37:35 -0700

    Update upsert-todo.ts

--- src/endpoints/todos/upsert-todo.ts
+++ src/endpoints/todos/upsert-todo.ts
@@ -1,7 +1,7 @@
 import { Endpoint } from "../../models/Endpoint.type";
 import { todosStore } from "../../datastore/TodosStore";
 
-export const upsertTodoUrl : string = '/todos/upsert-todo';
+export const upsertTodoUrl : string = '/todos/new-todo';
 
 const upsertTodoEndpointHandler = async (req, res, next) => {
     const todo = req.body;
